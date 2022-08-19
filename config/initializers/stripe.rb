115724e5a038db5a94077b29e491799e788fedcf
shahryarfareedkhan <shahryarfreed123@gmail.com>
2022-08-19 16:08:33 +0500

    stripe added

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+# Rails.configuration.stripe = {
+#     :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+#     :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+# }   
+
+Stripe.api_key = 'sk_test_51LXjzeJUi9tWZPTDtN8wNs23IPQw76JMp9AOSp0IX2tlAfiHGSRZGhXrwg2Elqiaxx2b6bgjw5KL4eNODEDaVdaX00Qb3IEgk2'
