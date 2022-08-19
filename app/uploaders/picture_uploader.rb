2ef0d66404f9e6f6a758337ccb544f5e1ccbaff6
shahryarfareedkhan <shahryarfreed123@gmail.com>
2022-08-19 21:54:56 +0500

    pic_uploader changing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,12 +6,11 @@ process resize_to_limit: [300, 300]
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
   if Rails.env.production?
-    storage :file
-  else
     storage :fog
   end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -40,13 +39,13 @@ process resize_to_limit: [300, 300]
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+    def extension_allowlist
+      %w(jpg jpeg gif png)
+    end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-    def filename
-      "something.jpg" if original_filename
-    end
+    # def filename
+    #   "something.jpg" if original_filename
+    # end
 end
