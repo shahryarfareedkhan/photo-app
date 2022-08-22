fe0e5635ea0026bc03d9f71664b63cdef5abad5b
shahryarfareedkhan <shahryarfreed123@gmail.com>
2022-08-22 15:26:54 +0500

    test 2 aws

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,7 +9,7 @@ process resize_to_limit: [300, 300]
   storage :file
   # storage :fog
   if Rails.env.production?
-    storage :amazon
+    storage :fog
   end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
